b94784dc04fc8ad28db9e8ffaaf74ecca26c90da
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-11-04 21:50:59 +0800

    t

--- database/migrations/2025_11_04_083238_create_subjects_table.php
+++ database/migrations/2025_11_04_083238_create_subjects_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('semester_id')->constrained('semesters')->onDelete('cascade');
+            $table->string('subject_code');
+            $table->string('des_title');
+            $table->integer('total_units');
+            $table->integer('lec_units');
+            $table->integer('lab_units');
+            $table->integer('total_hrs');
+            $table->integer('total_lec_hrs');
+            $table->integer('total_lab_hrs');
+            $table->string('pre_requisite')->nullable();
             $table->timestamps();
         });
     }
